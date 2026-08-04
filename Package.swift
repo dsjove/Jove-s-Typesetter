6e213b35fb7fdafcffbc44dad52a1aba305c1ca8
David Giovannini <dsjove@mac.com>
2026-08-03 20:16:56 -0500

    change package name

--- Package.swift
+++ Package.swift
@@ -4,26 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "Jove's Typesetter",
+    name: "SBJLayout",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Jove's Typesetter",
-            targets: ["Jove_s_Typesetter"]
+            name: "SBJLayout",
+            targets: ["SBJLayout"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Jove_s_Typesetter",
+            name: "SBJLayout",
             swiftSettings: [
                 .enableUpcomingFeature("ApproachableConcurrency"),
             ],
         ),
         .testTarget(
-            name: "Jove_s_TypesetterTests",
-            dependencies: ["Jove_s_Typesetter"],
+            name: "SBJLayoutTests",
+            dependencies: ["SBJLayout"],
             swiftSettings: [
                 .enableUpcomingFeature("ApproachableConcurrency"),
             ],
